688e1f7349b173801de0145f5cd3c3fd5800f4d6
Rick Mangi <rick.mangi@datadoghq.com>
2022-12-02 12:44:47 -0500

    day2

--- src/rmangi/advent.clj
+++ src/rmangi/advent.clj
@@ -1,7 +1,8 @@
 (ns rmangi.advent
   (:gen-class)
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [rmangi.day1 :refer [run-day-1]]))
+            [rmangi.day1 :as day1]
+            [rmangi.day2 :as day2]))
 
 (def cli-options
   [["-d" "--day DAY" "which day"
@@ -15,6 +16,7 @@
         day (get-in opts [:options :day])]
     (if (nil? (:errors opts))
       (condp = day
-        1 (run-day-1)
+        1 (day1/run-day)
+        2 (day2/run-day)
         (println "not yet!"))
       (println "Got errors parsing: " (:errors opts)))))
